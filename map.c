63dbb4525edffd0c3a3be5cabfa074365103a1c3
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-04-19 20:30:04 +0300

    Update map.c

--- map.c
+++ map.c
@@ -40,7 +40,161 @@ Map mapCreate(copyMapDataElements copyDataElement,copyMapKeyElements copyKeyElem
     map->compare_keys = compareKeyElements;
     return map;
 }
+//destroy 
+void mapDestroy(Map map){
+    if (map==NULL){
+        return;
+    }
+    mapClear(map);
+    free(map);
+}
 
+//mapGetSize
+int mapGetSize(Map map){
+    if (map==NULL){
+        return EMPTY;
+    }
+    return map->size;
+}
+
+
+//mapPut
+static Node node_Create (Map map ,MapKeyElement keyElement,  MapDataElement dataElement){
+    assert(!map||!keyElement||!dataElement);
+    Node node =malloc(sizeof(*node));
+    if (node==NULL){
+        return NULL;
+    }
+    node->next=NULL;
+    node->previous=NULL;
+    node->key=map->copy_key(keyElement);
+    if (node->key==NULL){
+        free(node);
+        return NULL;
+    }
+    node->data=map->copy_data(dataElement);
+    if (node->data==NULL){
+        map->free_key(node->key);
+        free(node);
+        return NULL;
+    }
+    return node;
+}
+static MapResult edit_Data(Map map ,MapKeyElement keyElement,  MapDataElement dataElement){
+    mapGetFirst(map);
+    Node ptr =map->iterator;
+    while (ptr!=NULL){
+        if (map->compare_keys(keyElement  , ptr->key)==0){
+            MapDataElement data = map->copy_data(dataElement);
+            if (data==NULL){
+                return MAP_OUT_OF_MEMORY;
+            }
+            map->free_data(map->iterator->data);
+            ptr->data=data;
+            break;
+        }
+        ptr=ptr->next;
+    }
+    return MAP_SUCCESS;
+}
+static MapResult add_key(Map map ,MapKeyElement key ,MapDataElement data){
+    Node node = node_Create(map,key,data);
+    if (node==NULL){
+        return MAP_OUT_OF_MEMORY;
+    }
+    if (map->size==0){
+        map->iterator=node;
+        map->size++;
+        return MAP_SUCCESS;
+    }
+    mapGetFirst(map);
+    Node ptr = map->iterator;
+    while (ptr!=NULL){
+        if (map->compare_keys(ptr->key,key)>0){
+            node->next=ptr;
+            if(ptr->previous==NULL){
+                node->previous=NULL;
+                ptr->previous=node;
+
+            } else{
+            ptr=ptr->previous;
+            ptr->next=node;
+            node->previous=ptr;
+            ptr=ptr->next->next;
+            ptr->previous=node;
+            }
+            map->size++;
+            return MAP_SUCCESS;
+        }
+        ptr=ptr->next;
+    }
+    while(map->iterator->next!=NULL){
+        map->iterator=map->iterator->next;
+    }
+    map->iterator->next=node;
+    node->previous=map->iterator;
+    map->size++;
+    return MAP_SUCCESS;
+
+}
+MapResult mapPut(Map map, MapKeyElement keyElement, MapDataElement dataElement){
+    if (map==NULL){
+        return MAP_NULL_ARGUMENT;
+    }
+    if (mapContains(map,keyElement)==0) {
+        MapResult result = edit_Data(map, keyElement, dataElement);
+        return result;
+    }
+    MapResult result=add_key(map,keyElement,dataElement);
+    return result;
+
+}
+
+
+//mapRemove
+MapResult mapRemove(Map map, MapKeyElement keyElement){
+    if(map==NULL){
+        return MAP_NULL_ARGUMENT;
+    }
+    if (!mapContains(map,keyElement)){
+        return MAP_ITEM_DOES_NOT_EXIST;
+    }
+    if (map->size <=1){
+        return  mapClear(map);
+    }
+    while (map->iterator->next!=NULL){
+        if (map->compare_keys(keyElement,map->iterator->key)==0){
+            map->free_data(map->iterator->data);
+            map->free_key(map->iterator->key);
+            if(map->iterator->previous==NULL) {
+                map->iterator=map->iterator->next;
+                map->iterator->previous=NULL;
+            } else{
+                map->iterator=map->iterator->next;
+                map->iterator->previous=map->iterator->previous->previous;
+                map->iterator= map->iterator->previous->previous;
+                map->iterator->next=map->iterator->next->next;
+            }
+            map->size--;
+            return MAP_SUCCESS;
+        }
+        map->iterator=map->iterator->next;
+    }
+    map->iterator=map->iterator->previous;
+    map->iterator->next=NULL;
+    map->size--;
+    return MAP_SUCCESS;
+}
+
+
+//mapGetNext
+MapKeyElement mapGetNext(Map map){
+    if (map==NULL|| map->iterator==NULL||map->iterator->next==NULL){
+        return NULL;
+    }
+    map->iterator=map->iterator->next;
+    return map->iterator->key;
+}
 
 ///////////////////////YASMEEN'S CODE GOES HERE/////////////////////////////
 
