438c6f966716e5abc1a71d5eb42b261dcd9c35da
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-04-19 21:57:09 +0300

    Create cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/ex1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex1.dir/map.c.obj"
+  "ex1.pdb"
+  "ex1.exe"
+  "ex1.exe.manifest"
+  "ex1.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ex1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
