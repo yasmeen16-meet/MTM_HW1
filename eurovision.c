9d9ef6ed0ba44bb145aa3c15c5f293ad86bbd1d9
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-04-28 22:04:39 +0300

    Update eurovision.c

--- eurovision.c
+++ eurovision.c
@@ -11,3 +11,23 @@ struct eurovision_t{
     State states;
     Judge judges;
 };
+static bool isValidName(char * name);
+
+
+
+
+
+////functions application
+
+///checking the validity of a given name: small letter or space only
+static bool isValidName(char * name) {
+    assert(name != NULL);
+    int i = 0;
+    while (name[i]) {
+        if (name[i] != ' ' || name[i] < 'a' || name[i] > 'z') {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
