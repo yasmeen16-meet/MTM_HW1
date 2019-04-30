2ae76609ed6624197670d97d86ea097a5f5af273
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-04-30 21:05:26 +0300

    Update eurovision.c

--- eurovision.c
+++ eurovision.c
@@ -14,12 +14,37 @@ struct eurovision_t{
 };
 
 static bool isValidName(char * name);
-
-
+static int rankToScore(int rank);
 
 
 
 ////functions application
+
+///rankToScore function
+static int rankToScore(int rank){
+
+    switch (rank){
+        case 0:
+            return 12;
+        case 1:
+            return 10;
+        case 2:
+            return 8;
+        case 3:
+            return 7;
+        case 4:
+            return 6;
+        case 5:
+            return 5;
+        case 6:
+            return 4;
+        case 7:
+            return 3;
+        case 8:
+            return 2;
+    }
+    return 1;
+}
 
 ///checking the validity of a given name: small letter or space only
 static bool isValidName(char * name) {
