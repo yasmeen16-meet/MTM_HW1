ff49f8ed2237524641c8ef5f76a8a18299e4d6d1
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-05-01 13:45:41 +0300

    Update eurovision.c

--- eurovision.c
+++ eurovision.c
@@ -93,6 +93,155 @@ EurovisionResult eurovisionAddVote(Eurovision eurovision, int stateGiver, int st
     return EUROVISION_SUCCESS;
 
 }
+
+
+////////////////////////////////YASMEEN'S CODE GOES HERE//////////////////////////////////////
+
+
+///eurovisionDestroy function:
+///destroying a whole eurovision program: state & judges
+void eurovisionDestroy(Eurovision eurovision){
+    if (eurovision == NULL){
+        return;
+    }
+    stateDestroy(eurovision->states);
+    judgeDestroy(eurovision->judges);
+    free(eurovision);
+}
+
+
+///eurovisionAddJudge function:
+///adding a new judge to the eurovision program, including his votes details
+///checking the validity of a given name: small letter or space only
+static bool isValidName(const char * name) {
+    assert(name != NULL);
+    int i = 0;
+    while (name[i]) {
+        if (name[i] != ' ' || name[i] < 'a' || name[i] > 'z') {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+
+EurovisionResult eurovisionAddJudge(Eurovision eurovision, int judgeId,
+                                    const char *judgeName,
+                                    int *judgeResults) {
+    if (eurovision == NULL || judgeName == NULL || judgeResults == NULL) {
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    ///checking id validity
+    if (judgeId < ZERO) {
+        return EUROVISION_INVALID_ID;
+    }
+    for (int i=0; i<LEN;i++) {
+        if (judgeResults[i] < ZERO) {
+            return EUROVISION_INVALID_ID;
+        }
+    }
+    ///checking if a stateId is repeated
+    for (int i=0; i<LEN-1;i++) {
+        for (int j=i+1; j<LEN; j++){
+            if (judgeResults[i] == judgeResults[j]){
+                return EUROVISION_INVALID_ID;
+            }
+        }
+    }
+    ///judgeName validity
+    if (!isValidName(judgeName)) {
+        return EUROVISION_INVALID_NAME;
+    }
+    ///state's existence in the array judgeResults
+    for (int i = 0; i < LEN; i++) {
+        int current_stateId = judgeResults[i];
+        if (!stateContain(eurovision->states, current_stateId)) {
+            return EUROVISION_STATE_NOT_EXIST;
+        }
+    }
+    if (judgeContain(eurovision->judges, judgeId)) {
+        return EUROVISION_JUDGE_ALREADY_EXIST;
+    }
+    ///All parameters are legal, we can try to add the judge
+    JudgeResult result = judgeAdd(eurovision->judges, judgeId, judgeName, judgeResults);
+    ///checking results from judgeAdd function
+    if (result == JUDGE_NULL_ARGUMENT) {
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    if (result == JUDGE_OUT_OF_MEMORY) {
+        eurovisionDestroy(eurovision);
+        return EUROVISION_OUT_OF_MEMORY;
+    }
+    return EUROVISION_SUCCESS;
+}
+
+EurovisionResult eurovisionRemoveJudge(Eurovision eurovision, int judgeId){
+    if (eurovision == NULL){
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    if (judgeId<ZERO){
+        return EUROVISION_INVALID_ID;
+    }
+    if (!judgeContain(eurovision->judges, judgeId)){
+        return EUROVISION_JUDGE_NOT_EXIST;
+    }
+    ///All parameters are legal, we can try to remove the judge
+    JudgeResult result = judgeRemove(eurovision->judges, judgeId);
+    if(result == JUDGE_NULL_ARGUMENT){
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    return EUROVISION_SUCCESS;
+}
+
+
+EurovisionResult eurovisionRemoveVote(Eurovision eurovision, int stateGiver,
+                                      int stateTaker) {
+    if (eurovision == NULL) {
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    if (stateGiver < ZERO || stateTaker < ZERO) {
+        return EUROVISION_INVALID_ID;
+    }
+    if (!stateContain(eurovision->states, stateGiver) || !stateContain(eurovision->states, stateTaker)) {
+        return EUROVISION_STATE_NOT_EXIST;
+    }
+    if (stateGiver == stateTaker) {
+        return EUROVISION_SAME_STATE;
+    }
+    ///All parameters are legal, we can try to remove a vote from stateTaker
+    State ptrvoter = stateFind(eurovision->states, stateGiver);
+    Map map = ptrvoter->stateVotes;
+    ///option 1: stateGiver has not voted to stateTaker in the first place.
+    if (!mapContains(map, (int*) stateTaker)) {
+        return EUROVISION_SUCCESS;
+    }
+    ///option 2: stateGiver has voted to stateTaker before
+    //reaching the map cell with votes to stateTaker
+    int* currentKey = mapGetFirst(map);
+    while (currentKey != NULL) {
+        if (*currentKey == stateTaker) {
+            break;
+        }
+        currentKey=mapGetNext(map);
+    }
+
+    ///suboption 2.1: there is 1 vote only to remove, thus we remove the votes details of this stateTaker
+    if (*(int *) mapGet(map,currentKey) == 1) {
+        mapRemove(map, currentKey);
+        return EUROVISION_SUCCESS;
+    }
+    ///suboption 2.2: there are more than one vote, thus we reduce the votes number by 1. using mapPut
+    int data=*(int*)mapGet(map, currentKey) - 1;
+    MapResult result = mapPut(map,currentKey, (int*)data);
+    if (result == MAP_NULL_ARGUMENT){
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    return EUROVISION_SUCCESS;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 ///rankToScore function
 static int rankToScore(int rank){
 
