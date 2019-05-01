28188829ae39b1e2c01c970fc2bb9d13275ce4b3
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-05-01 13:58:09 +0300

    Update eurovision.c

--- eurovision.c
+++ eurovision.c
@@ -2,22 +2,48 @@
 #include <assert.h>
 #include <stdlib.h>
 #include "../eurovision.h"
-#include "state.c"
-#include "judge.c"
+#include "../state.c"
+#include "../judge.c"
 #include "../list.h"
-
+#define ZERO 0
 
 struct eurovision_t{
     State states;
     Judge judges;
 };
 
-static bool isValidName(char * name);
+static bool isValidName(const char * name);
 static int rankToScore(int rank);
 
 
 
 ////functions application
+
+///rankToScore function
+static int rankToScore(int rank){
+
+    switch (rank){
+        case 0:
+            return 12;
+        case 1:
+            return 10;
+        case 2:
+            return 8;
+        case 3:
+            return 7;
+        case 4:
+            return 6;
+        case 5:
+            return 5;
+        case 6:
+            return 4;
+        case 7:
+            return 3;
+        case 8:
+            return 2;
+    }
+    return 1;
+}
 EurovisionResult eurovisionAddState(Eurovision eurovision, int stateId, const char *stateName, const char *songName){
     if (eurovision==NULL || stateName ==NULL || songName==NULL){
         return EUROVISION_NULL_ARGUMENT;
@@ -93,13 +119,6 @@ EurovisionResult eurovisionAddVote(Eurovision eurovision, int stateGiver, int st
     return EUROVISION_SUCCESS;
 
 }
-
-
-////////////////////////////////YASMEEN'S CODE GOES HERE//////////////////////////////////////
-
-
-///eurovisionDestroy function:
-///destroying a whole eurovision program: state & judges
 void eurovisionDestroy(Eurovision eurovision){
     if (eurovision == NULL){
         return;
@@ -109,9 +128,6 @@ void eurovisionDestroy(Eurovision eurovision){
     free(eurovision);
 }
 
-
-///eurovisionAddJudge function:
-///adding a new judge to the eurovision program, including his votes details
 ///checking the validity of a given name: small letter or space only
 static bool isValidName(const char * name) {
     assert(name != NULL);
@@ -124,7 +140,6 @@ static bool isValidName(const char * name) {
     }
     return true;
 }
-
 
 EurovisionResult eurovisionAddJudge(Eurovision eurovision, int judgeId,
                                     const char *judgeName,
@@ -238,45 +253,4 @@ EurovisionResult eurovisionRemoveVote(Eurovision eurovision, int stateGiver,
         return EUROVISION_NULL_ARGUMENT;
     }
     return EUROVISION_SUCCESS;
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-///rankToScore function
-static int rankToScore(int rank){
-
-    switch (rank){
-        case 0:
-            return 12;
-        case 1:
-            return 10;
-        case 2:
-            return 8;
-        case 3:
-            return 7;
-        case 4:
-            return 6;
-        case 5:
-            return 5;
-        case 6:
-            return 4;
-        case 7:
-            return 3;
-        case 8:
-            return 2;
-    }
-    return 1;
-}
-
-///checking the validity of a given name: small letter or space only
-static bool isValidName(char * name) {
-    assert(name != NULL);
-    int i = 0;
-    while (name[i]) {
-        if (name[i] != ' ' || name[i] < 'a' || name[i] > 'z') {
-            return false;
-        }
-        i++;
-    }
-    return true;
-}
+}}
