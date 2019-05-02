f5404297dab96bf728beeaa129d98fabbdfdb20b
dalalEg <49488556+dalalEg@users.noreply.github.com>
2019-05-02 13:10:50 +0300

    Update eurovision.c

--- eurovision.c
+++ eurovision.c
@@ -14,36 +14,12 @@ struct eurovision_t{
 
 static bool isValidName(const char * name);
 static int rankToScore(int rank);
-
-
+static int findScore (int* scoreArray ,int stateId);
+static EurovisionResult updateStatesScores (State state);
+static EurovisionResult updateJudgeScores (Judge judge , State state);
+static EurovisionResult calculateFinalScore (State state , int audiencePercent);
 
 ////functions application
-
-///rankToScore function
-static int rankToScore(int rank){
-
-    switch (rank){
-        case 0:
-            return 12;
-        case 1:
-            return 10;
-        case 2:
-            return 8;
-        case 3:
-            return 7;
-        case 4:
-            return 6;
-        case 5:
-            return 5;
-        case 6:
-            return 4;
-        case 7:
-            return 3;
-        case 8:
-            return 2;
-    }
-    return 1;
-}
 
 Eurovision eurovisionCreate(){
 
@@ -53,81 +29,7 @@ Eurovision eurovisionCreate(){
     }
     return eurovision;
 }
-EurovisionResult eurovisionAddState(Eurovision eurovision, int stateId, const char *stateName, const char *songName){
-    if (eurovision==NULL || stateName ==NULL || songName==NULL){
-        return EUROVISION_NULL_ARGUMENT;
-    }
-    if (stateId < 0){
-        return EUROVISION_INVALID_ID;
-    }
-    if (!isValidName(stateName)||!isValidName(songName)){
-        return EUROVISION_INVALID_NAME;
-    }
-    if(stateContain(eurovision->states,stateId)){
-        return EUROVISION_STATE_ALREADY_EXIST;
-    }
-    StateResult result = stateAdd(eurovision->states,stateId,stateName,songName);
-    if (result == STATE_OUT_OF_MEMORY){
-        //  eurovisionDestroy(eurovision);
-        return EUROVISION_OUT_OF_MEMORY;
-    }
-    return EUROVISION_SUCCESS;
-}
 
-EurovisionResult eurovisionRemoveState(Eurovision eurovision, int stateId){
-    if (eurovision==NULL ){
-        return  EUROVISION_NULL_ARGUMENT;
-    }
-    if (stateId < 0){
-        return EUROVISION_INVALID_ID;
-    }
-    if (!stateContain(eurovision->states,stateId)){
-        return EUROVISION_STATE_NOT_EXIST;
-    }
-    judgeRemoveState(eurovision->judges, stateId);
-    deleteOutState(eurovision->states,stateId);
-    stateRemove(eurovision->states,stateId);
-    return EUROVISION_SUCCESS;
-}
-
-EurovisionResult eurovisionAddVote(Eurovision eurovision, int stateGiver, int stateTaker) {
-    if (eurovision == NULL) {
-        return EUROVISION_NULL_ARGUMENT;
-    }
-    if (stateGiver < 0 || stateTaker) {
-        return EUROVISION_INVALID_ID;
-    }
-    if (!stateContain(eurovision->states, stateGiver) || !stateContain(eurovision->states, stateTaker)) {
-        return EUROVISION_STATE_NOT_EXIST;
-    }
-    if (stateGiver == stateTaker) {
-        return EUROVISION_SAME_STATE;
-    }
-    State stateGive = stateFind(eurovision->states, stateGiver);
-    Map map = stateGive->stateVotes;
-    if (mapContains(map, (int*)stateTaker)) {
-        MapResult result = mapPut(map, (int *) stateTaker, mapGet(map, (int *) (stateTaker + 1)));
-        if (result == MAP_OUT_OF_MEMORY) {
-            // eurovisionDestroy(eurovision);
-            return EUROVISION_OUT_OF_MEMORY;
-        }
-        if (result == MAP_NULL_ARGUMENT) {
-            return EUROVISION_NULL_ARGUMENT;
-        }
-    } else {
-        int j = 0;
-        MapResult result = mapPut(map, (int *) stateTaker, mapGet(map, (int *) (j + 1)));
-        if (result == MAP_OUT_OF_MEMORY) {
-            // eurovisionDestroy(eurovision);
-            return EUROVISION_OUT_OF_MEMORY;
-        }
-        if (result == MAP_NULL_ARGUMENT) {
-            return EUROVISION_NULL_ARGUMENT;
-        }
-    }
-    return EUROVISION_SUCCESS;
-
-}
 void eurovisionDestroy(Eurovision eurovision){
     if (eurovision == NULL){
         return;
@@ -151,6 +53,28 @@ static bool isValidName(const char * name) {
     }
     return true;
 }
+
+EurovisionResult eurovisionAddState(Eurovision eurovision, int stateId, const char *stateName, const char *songName){
+    if (eurovision==NULL || stateName ==NULL || songName==NULL){
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    if (stateId < 0){
+        return EUROVISION_INVALID_ID;
+    }
+    if (!isValidName(stateName)||!isValidName(songName)){
+        return EUROVISION_INVALID_NAME;
+    }
+    if(stateContain(eurovision->states,stateId)){
+        return EUROVISION_STATE_ALREADY_EXIST;
+    }
+    StateResult result = stateAdd(eurovision->states,stateId,stateName,songName);
+    if (result == STATE_OUT_OF_MEMORY){
+        //  eurovisionDestroy(eurovision);
+        return EUROVISION_OUT_OF_MEMORY;
+    }
+    return EUROVISION_SUCCESS;
+}
+
 EurovisionResult eurovisionAddJudge(Eurovision eurovision, int judgeId,
                                     const char *judgeName,
                                     int *judgeResults) {
@@ -201,6 +125,22 @@ EurovisionResult eurovisionAddJudge(Eurovision eurovision, int judgeId,
     return EUROVISION_SUCCESS;
 }
 
+EurovisionResult eurovisionRemoveState(Eurovision eurovision, int stateId){
+    if (eurovision==NULL ){
+        return  EUROVISION_NULL_ARGUMENT;
+    }
+    if (stateId < 0){
+        return EUROVISION_INVALID_ID;
+    }
+    if (!stateContain(eurovision->states,stateId)){
+        return EUROVISION_STATE_NOT_EXIST;
+    }
+    judgeRemoveState(eurovision->judges, stateId);
+    deleteOutState(eurovision->states,stateId);
+    stateRemove(eurovision->states,stateId);
+    return EUROVISION_SUCCESS;
+}
+
 EurovisionResult eurovisionRemoveJudge(Eurovision eurovision, int judgeId){
     if (eurovision == NULL){
         return EUROVISION_NULL_ARGUMENT;
@@ -217,6 +157,46 @@ EurovisionResult eurovisionRemoveJudge(Eurovision eurovision, int judgeId){
         return EUROVISION_NULL_ARGUMENT;
     }
     return EUROVISION_SUCCESS;
+}
+
+
+EurovisionResult eurovisionAddVote(Eurovision eurovision, int stateGiver, int stateTaker) {
+    if (eurovision == NULL) {
+        return EUROVISION_NULL_ARGUMENT;
+    }
+    if (stateGiver < 0 || stateTaker) {
+        return EUROVISION_INVALID_ID;
+    }
+    if (!stateContain(eurovision->states, stateGiver) || !stateContain(eurovision->states, stateTaker)) {
+        return EUROVISION_STATE_NOT_EXIST;
+    }
+    if (stateGiver == stateTaker) {
+        return EUROVISION_SAME_STATE;
+    }
+    State stateGive = stateFind(eurovision->states, stateGiver);
+    Map map = stateGive->stateVotes;
+    if (mapContains(map, (int*)stateTaker)) {
+        MapResult result = mapPut(map, (int *) stateTaker, mapGet(map, (int *) (stateTaker + 1)));
+        if (result == MAP_OUT_OF_MEMORY) {
+            // eurovisionDestroy(eurovision);
+            return EUROVISION_OUT_OF_MEMORY;
+        }
+        if (result == MAP_NULL_ARGUMENT) {
+            return EUROVISION_NULL_ARGUMENT;
+        }
+    } else {
+        int j = 0;
+        MapResult result = mapPut(map, (int *) stateTaker, mapGet(map, (int *) (j + 1)));
+        if (result == MAP_OUT_OF_MEMORY) {
+            // eurovisionDestroy(eurovision);
+            return EUROVISION_OUT_OF_MEMORY;
+        }
+        if (result == MAP_NULL_ARGUMENT) {
+            return EUROVISION_NULL_ARGUMENT;
+        }
+    }
+    return EUROVISION_SUCCESS;
+
 }
 
 
@@ -264,3 +244,106 @@ EurovisionResult eurovisionRemoveVote(Eurovision eurovision, int stateGiver,
     }
     return EUROVISION_SUCCESS;
 }
+
+///rankToScore function
+static int rankToScore(int rank){
+
+    switch (rank){
+        case 0:
+            return 12;
+        case 1:
+            return 10;
+        case 2:
+            return 8;
+        case 3:
+            return 7;
+        case 4:
+            return 6;
+        case 5:
+            return 5;
+        case 6:
+            return 4;
+        case 7:
+            return 3;
+        case 8:
+            return 2;
+    }
+    return 1;
+}
+
+/// returns the score if not found returns 0
+static int findScore (int* scoreArray ,int stateId){
+    //assert
+    int i=0;
+    while (i<LEN){
+        if (scoreArray[i]==stateId){
+            return rankToScore(i);
+        }
+        i++;
+    }
+    return ZERO;
+}
+
+///updates the stateScores according to the state Array
+static EurovisionResult updateStatesScores (State state){
+    assert(state!=NULL);
+    State  help_iterator = state;
+    while (help_iterator!=NULL){
+        State help2 = state;
+        double  sum =0 ;
+        while (help2!=NULL){
+            if (help_iterator->stateId==help2->stateId){
+                continue;
+            }
+            int current=findScore(help2->stateVotedScores,help_iterator->stateId);
+            if (current>0){
+                sum=sum+current;
+            }
+            help2=help2->stateNext;
+        }
+        help_iterator->stateStatesScore=sum/(stateGetSize(state)-1);
+        help_iterator= help_iterator->stateNext;
+    }
+    return  EUROVISION_SUCCESS;
+}
+
+/// updates average the state score according to the judge array
+static EurovisionResult updateJudgeScores (Judge judge , State state){
+    if (judge ==NULL || state==NULL){
+        return  EUROVISION_NULL_ARGUMENT;
+    }
+
+    State  help_iterator = state;
+    while (help_iterator!=NULL){
+        Judge help2 = judge;
+        double  sum =0 ;
+        while (help2!=NULL){
+            int current=findScore(help2->judgeResult,help_iterator->stateId);
+            if (current>0){
+                sum=sum+current;
+            }
+            help2=help2->judgeNext;
+        }
+        help_iterator->stateJudgesScore=sum/(judgeGetSize(judge));
+        help_iterator= help_iterator->stateNext;
+    }
+    return  EUROVISION_SUCCESS;
+}
+
+///calculate weightedScores for each state
+static EurovisionResult calculateFinalScore (State state , int audiencePercent){
+    assert(state!=NULL);
+    State  help_iterator =state;
+    while (help_iterator!= NULL){
+        int judgePercent = 100-audiencePercent;
+        help_iterator->stateWeightedScore= help_iterator->stateStatesScore*(audiencePercent/100.0)+
+                                           help_iterator->stateJudgesScore*(judgePercent/100.0);
+        help_iterator= help_iterator->stateNext;
+
+    }
+    return EUROVISION_SUCCESS;
+}
+
+
+
+
