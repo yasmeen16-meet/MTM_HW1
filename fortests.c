9947a30f1a7a616e5c5c3ee035ce680be7c6f630
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-05-03 21:00:19 +0300

    Update fortests.c

--- fortests.c
+++ fortests.c
@@ -15,3 +15,36 @@ static MapResult edit_Data(Map* map, Node* iterator ,MapKeyElement keyElement,  
     }
     return MAP_SUCCESS;
 }
+
+
+
+
+static int getMaxIdMap(Map* map_copy) {
+    assert(map_copy != NULL);
+
+    mapGetFirst(*map_copy);
+    if ((*map_copy)->iterator==NULL){
+        return NOMOREMAX;
+    }
+    int max_id = NOMOREMAX;
+    int max_votes = 0;
+    while ((*map_copy)->iterator->next!=NULL){
+        if (*((int*)(*map_copy)->iterator->data)> max_votes){
+            max_votes = *((int*)(*map_copy)->iterator->data);
+            max_id = *((int*) (*map_copy)->iterator->key);
+        }else if (*((int*)(*map_copy)->iterator->data)== max_votes){
+            max_id =  *((int*) (*map_copy)->iterator->key) < max_id?
+                      *((int*) (*map_copy)->iterator->key):max_id;
+        }
+        (*map_copy)->iterator = (*map_copy)->iterator->next;
+    }
+
+    if (*((int*)(*map_copy)->iterator->data)> max_votes){
+        max_votes = *((int*)(*map_copy)->iterator->data);
+        max_id = *((int*) (*map_copy)->iterator->key);
+    }else if (*((int*)(*map_copy)->iterator->data)== max_votes){
+        max_id =  *((int*) (*map_copy)->iterator->key) < max_id?
+                  *((int*) (*map_copy)->iterator->key):max_id;
+    }
+    return max_id;
+}
