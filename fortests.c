8dd35f2af0a789a5f334709e2a92e65384fd5609
yasmeen16-meet <yasmeensaadi99@gmail.com>
2019-05-04 20:31:19 +0300

    Update fortests.c

--- fortests.c
+++ fortests.c
@@ -217,3 +217,38 @@ State stateCreate(int stateId, const char* stateName, const char* songName) {
     new_state->stateNext=NULL;
     return new_state;
 }
+
+
+
+
+
+
+//////הכרזה//////
+char* strcpy(char* destination, const char* source);
+
+
+////מימוש////////
+char* strcpy(char* destination, const char* source)
+{
+	// return if no memory is allocated to the destination
+	if (destination == NULL)
+		return NULL;
+
+	// take a pointer pointing to the beginning of destination string
+	char *ptr = destination;
+	
+	// copy the C-string pointed by source into the array
+	// pointed by destination
+	while (*source != '\0')
+	{
+		*destination = *source;
+		destination++;
+		source++;
+	}
+
+	// include the terminating null character
+	*destination = '\0';
+
+	// destination is returned by standard strcpy()
+	return ptr;
+}
